f2cff0a54d9ae78809145cd25f345b18b4bd3d23
Sanket Dahegaonkar <dahegaonkarsanket@gmail.com>
2019-04-20 00:48:14 +0530

    Updates

---
+++ precache-manifest.527613b87929a1f792bacfc93f690e33.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c9c89fb223d956ab7e",
+    "url": "/quiz-wiz/static/js/main.58fbcf76.chunk.js"
+  },
+  {
+    "revision": "762a420fc860830667c1",
+    "url": "/quiz-wiz/static/js/runtime~main.00616452.js"
+  },
+  {
+    "revision": "4dadb5bee66db8755d2d",
+    "url": "/quiz-wiz/static/js/2.6bff8d87.chunk.js"
+  },
+  {
+    "revision": "9cd0c41ef52042ec8c718fbadb0484cd",
+    "url": "/quiz-wiz/index.html"
+  }
+];
